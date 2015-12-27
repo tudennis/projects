da77d3384f2364eec383b9e6f2d4ccce0dc49db4
Dennis <tudennis@gmail.com>
2015-12-27 12:59:02 -0500

    power

---
+++ power/power.cpp
@@ -0,0 +1,28 @@
+/*
+Implement pow(x, n)
+*/
+
+#include <iostream>
+
+double pow(double x, int n)
+{
+	double result;
+
+	return result;
+}
+
+int main(int argc, char ** argv)
+{
+	double input;
+	int power;
+
+	std::cout << "Please input a number and return: ";
+	std::cin << input;
+
+	std::cout << "Please input the power and return: ";
+	std::cin << power;
+
+	std::cout << "The number is :" << input << ", and the power is : " << power << std::endl;
+
+	return 0;
+}
